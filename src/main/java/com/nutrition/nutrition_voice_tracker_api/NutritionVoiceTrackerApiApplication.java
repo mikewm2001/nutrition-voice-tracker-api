82fe359608eced8ab077775df858b711d12e3491
Michael Mei <michaelwmei09@gmail.com>
2026-01-26 23:53:58 -0800

    Initialize spring boot project

---
+++ src/main/java/com/nutrition/nutrition_voice_tracker_api/NutritionVoiceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition.nutrition_voice_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionVoiceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionVoiceTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrition/nutrition_voice_tracker_api/NutritionVoiceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrition.nutrition_voice_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionVoiceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
